edff703f31d366d864713291477c8e6f44286644
Ajllow <alow2025@ash.nl>
2025-10-09 18:20:14 +0200

    began third guessing game

--- CodingChallenge/src/GuessingGame.java
+++ CodingChallenge/src/GuessingGame.java
@@ -1,29 +1,24 @@
 import java.util.Scanner;
+public class GuessingGame {
+    public static void main(String[] args) {
+        int magicNumber = (int) (Math.random() * (99)) + 1;
+        int theGuess = 0;
+        int counter = 0;
+        Scanner sc = new Scanner(System.in);
 
-public class GuessingGame
-{
-    {
-        System.out.println("I will think of a number between 1 and 100");
-        System.out.println("and you will try to guess it.");
-        System.out.println("After each guess, I will give you a clue about");
-        System.out.println("whether the correct number is higher or lower.");
-        int number = (int) (math.random(100) * 1);
-        System.out.println("I'm thinking of a number between 1 and 100...");
-        Scanner scan = new Scanner(System.in);
-        int guess = scan.nextInt();
-        System.out.println("Your guess?" + guess);
-        if (guess < number)
+        while (theGuess != magicNumber)
         {
-            System.out.println("It's lower.");
-        }
-        if (guess > number)
-        {
-            System.out.println("It's higher.");
-        }
-        else if (guess = number)
-        {
-            System.out.println("You got it right in" + nGuesses + "guesses!");
-        }
+            System.out.println("I'm thinking of a number between 1 and 100...");
+            theGuess = sc.nextInt();
+            if (theGuess < magicNumber)
+                System.out.println("It's higher.");
+            if (theGuess > magicNumber)
+                System.out.println("It's lower.");
+            if (theGuess == magicNumber)
+            {
+                System.out.println("You got it right in " + (counter+1) + " guesses!");
 
+                counter++;
+        }
     }
-}
+}}
