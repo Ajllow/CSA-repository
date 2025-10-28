37119604b019b59827b08f407e725baaaa7e3a77
Ajllow <alow2025@ash.nl>
2025-10-28 18:45:42 +0100

    finished sell/validate ticket portion

--- CodingChallenge/src/OVhaarlemmerbus.java
+++ CodingChallenge/src/OVhaarlemmerbus.java
@@ -2,30 +2,34 @@ import java.util.Scanner;
 public class OVhaarlemmerbus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double price;
-        int age;
-        System.out.println("New Trip Segment");
+        double price = 0;
+
+        System.out.println("Sell/Validate Ticket");
         System.out.println("Ticket type [single/day]?");
         String ticketType = sc.nextLine();
-        if (ticketType.equalsIgnoreCase("single")){
-            price = 3;
+
         System.out.println("Age");
-        age = sc.nextInt();
-        if (age < 18){
-            price = 1.5;//50% off price
-        }
-        else if (age > 65){
-            price = 2.1;//30% off price
-        }
-    }
-        else if (ticketType.equalsIgnoreCase("day")){
-        price = 7;}
-        System.out.println("Hour");
+        int age = sc.nextInt();
+
+        System.out.println("Hour?");
         int hour = sc.nextInt();
-        if (hour == 7){//7-9 and 16-18
-            if (age > 18) {
-                price = price + .5;
+
+        if (ticketType.equalsIgnoreCase("single")) {
+            price = 3;
+            if (age < 18) {
+                price = 1.5;//50% off price
+            } else if (age > 65) {
+                price = 2.1;//30% off price
+            }
+        } else if (ticketType.equalsIgnoreCase("day")) {
+            price = 7;
+        }
+
+        if ((hour >= 7 && hour <= 9) || (hour >= 16 && hour <= 18)) {
+            if (age >= 18) {
+                price += 0.5;
             }
         }
+        System.out.println("Total price: " + price);
     }
 }
