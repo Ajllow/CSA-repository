f662e424e90d94cf252fd05988ecbc89b8e0c69e
Ajllow <alow2025@ash.nl>
2025-11-18 14:03:18 +0100

    finished sell/validate ticket portion

--- chatbot/src/Magpie.java
+++ chatbot/src/Magpie.java
@@ -57,6 +57,38 @@ public class Magpie
     {
         String response = "";
         response = getRandomResponse();
+            if (statement.indexOf("math") >= 0 || statement.indexOf("physics") >= 0 || statement.indexOf("science") >= 0)
+                response = "is that your favorite subject?";
+            else if (statement.indexOf("american school of the hague") >= 0)
+                response = "I love that school!";
         return response;
+    }
+    private int findPhrase(String statement, String goal, int startPos)
+    {
+        String phrase = statement.toLowerCase();
+        goal = goal.toLowerCase();
+        int position = phrase.indexOf(goal, startPos);
+        while (position >= 0)
+        {
+            String before = " ", after = " ";
+            if (position > 0)
+            {
+                before = phrase.substring(position - 1, position);
+            }
+            if (position + goal.length() < phrase.length())
+            {
+                after = phrase.substring(position + goal.length(),
+                        position + goal.length() + 1);
+            }
+
+            /* determine the values of index, before, and after at this point in the method. */
+            if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) &&
+                    ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
+            {
+                return position;
+            }
+            position = phrase.indexOf(goal, position + 1);
+        }
+        return -1;
     }
 }
